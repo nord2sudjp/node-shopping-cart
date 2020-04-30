73871a924554571cd7c5a026ae3511bb7050422a
maka y <nord2sudjp@gmail.com>
2020-04-30 17:13:45 +0900

    #13 Cart Views - cart view

--- routes/index.js
+++ routes/index.js
@@ -43,4 +43,15 @@ router.get("/add-to-cart/:id", async (req, res, next) => {
   }
 });
 
+router.get("/shopping-cart", (req, res, next) => {
+  if (!req.session.cart) {
+    return res.render("shop/shopping-cart", { product: null });
+  }
+  var cart = new Cart(req.session.cart);
+  res.render("shop/shopping-cart", {
+    products: cart.generateArray(),
+    totalPrice: cart.totalPrice,
+  });
+});
+
 module.exports = router;
